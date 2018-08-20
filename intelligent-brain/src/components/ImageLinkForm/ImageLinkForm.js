ee155e048357e706a2fc4dc2703404fa501fe0e6
Chaoran Wang <jason.cr.wang@gmail.com>
2018-08-20 20:42:02 +0930

    Connect Clarifai API, create PicureArea component

--- intelligent-brain/src/components/ImageLinkForm/ImageLinkForm.js
+++ intelligent-brain/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
-    return <div>
+const ImageLinkForm = ({onInputChange, onDetectChange}) => {
+    return <div> 
         <p className="f3">
           {"Detect faces in a picture. Please upload your picture here."}
         </p>
         <div className="center">
           <div className="form center pa4 br3 shadow-5">
-            <input className="f4 pa2 w-70 center" type="text" />
-            <button className="w-30 grow f4 link ph3 pv2 dib black bg-light-blue">
+            <input className="f4 pa2 w-70 center" type="text" onInput={onInputChange}/>
+            <button className="w-30 grow f4 link ph3 pv2 dib black bg-light-blue" onClick={onDetectChange}>
               Detect
             </button>
           </div>
---
+++ intelligent-brain/src/components/PictureArea/PictureArea.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PictureArea = ({imageURL}) => {
+    return <div className="center ma">
+        <div className="absolute mt2">
+            <img src={imageURL} alt="" width="500px" height="auto"/>
+        </div>
+      </div>;
+};
+
+export default PictureArea;
